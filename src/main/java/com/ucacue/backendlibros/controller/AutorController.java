b603c9919f26c7116031d3b07b29a9d66df4e126
sebas <sebascb16@yahoo.com>
2023-07-24 23:50:23 -0500

    Implementacion de Cors Policy

--- src/main/java/com/ucacue/backendlibros/controller/AutorController.java
+++ src/main/java/com/ucacue/backendlibros/controller/AutorController.java
@@ -6,15 +6,13 @@ import com.ucacue.backendlibros.model.Libro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class AutorController {
 
--- src/main/java/com/ucacue/backendlibros/controller/EstudianteController.java
+++ src/main/java/com/ucacue/backendlibros/controller/EstudianteController.java
@@ -4,6 +4,7 @@ import com.ucacue.backendlibros.infraestructure.repositorio.EstudianteRepository
 import com.ucacue.backendlibros.model.Estudiante;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @Controller
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class EstudianteController {
 
